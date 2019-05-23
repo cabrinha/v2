e02727ce0ebc99d318d096b78ee429021988ce14
Scott Cabrinha <scott@internaught.io>
2019-05-23 14:29:08 -0700

    init commit of bot, not a working state

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	// Setup our config file and read it
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error reading config file: %s", err))
+	}
+
+	pre = viper.GetString("prefix")
+}
+
+func main() {
+	goBot, err := discordgo.New("Bot " + viper.GetString("token"))
+	if err != nil {
+		fmt.Println("error creating discord session: ", err)
+	}
+
+	goBot.AddHandler(messageCreate)
+
+	err = goBot.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	goBot.Close()
+
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+}
